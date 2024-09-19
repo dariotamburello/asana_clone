e5b1be03d9068596e971ebb33df0cbdb5870c806
Dario Tamburello <dariotamburello@hotmail.com>
2024-09-19 21:39:29 +0200

    new update

---
+++ src/data/site-tree.js
@@ -0,0 +1,240 @@
+export const TREE = [
+    {
+        id: 'asana',
+        title: 'Asana',
+        links : [
+            {
+                subtitle: 'Inicio',
+                url: '#'
+            },
+            {
+                subtitle: 'Producto',
+                url: 'https://asana.com/es/product'
+            },
+            {
+                subtitle: 'Novedades',
+                url: 'https://asana.com/es/whats-new'
+            },
+            {
+                subtitle: 'Precios',
+                url: 'https://asana.com/es/pricing'
+            },
+            {
+                subtitle: 'Starter',
+                url: 'https://asana.com/es/plan/starter'
+            },
+            {
+                subtitle: 'Avanzado',
+                url: 'https://asana.com/es/plan/advanced'
+            },
+            {
+                subtitle: 'Enterprise',
+                url: 'https://asana.com/es/enterprise'
+            },
+            {
+                subtitle: 'Éxito del Cliente',
+                url: 'https://asana.com/es/customer-success'
+            },
+            {
+                subtitle: 'Plantillas de Asana',
+                url: 'https://asana.com/es/templates'
+            },
+            {
+                subtitle: 'Confianza y seguridad',
+                url: 'https://asana.com/es/trust'
+            },
+            {
+                subtitle: 'Estado',
+                url: 'https://status.asana.com/?_gl=1*1xybi7*_gcl_au*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*FPAU*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*_ga*MTM2OTA1MzA2MC4xNzI2MzExNDkw*_ga_J1KDXMCQTH*MTcyNjc2OTA0MS4yNS4xLjE3MjY3Njk0ODYuMzAuMC4xNTg4MzQwNjA1*_fplc*WjBwRkclMkZFS1dZaUM1Q0s5allFc3BCaGZnWkdraHVubUpya3JONzE1OEFiZWtIM3pHTmlBR3NZMHV3R3EyTW01Tjl4ZzFiZHklMkZuSnlGSDZselduVjRHUVFIaDlMRnEzc0pkanI1UEJLMjZObTU0Smg5Zjc3dTVUN0hPdzhqQSUzRCUzRA..'
+            }
+        ]
+    },
+    {
+        id: 'about-us',
+        title: 'Acerca de nosotros',
+        links : [
+            {
+                subtitle: 'Empresa',
+                url: 'https://asana.com/es/company'
+            },
+            {
+                subtitle: 'Equipo directivo',
+                url: 'https://asana.com/es/leadership'
+            },
+            {
+                subtitle: 'Clientes',
+                url: 'https://asana.com/es/customers'
+            },
+            {
+                subtitle: 'Diversidad',
+                url: 'https://asana.com/es/diversity-and-inclusion'
+            },
+            {
+                subtitle: 'Carreras',
+                url: 'https://asana.com/es/jobs'
+            },
+            {
+                subtitle: 'Prensa',
+                url: 'https://asana.com/es/press'
+            },
+            {
+                subtitle: 'Wavelength',
+                url: 'https://wavelength.asana.com/?_gl=1*1exerz4*_gcl_au*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*FPAU*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*_ga*MTM2OTA1MzA2MC4xNzI2MzExNDkw*_ga_J1KDXMCQTH*MTcyNjc2NTgyNS4yNC4xLjE3MjY3NjY0MTIuMC4wLjE4MjQ4OTE3MzU.*_fplc*WjBwRkclMkZFS1dZaUM1Q0s5allFc3BCaGZnWkdraHVubUpya3JONzE1OEFiZWtIM3pHTmlBR3NZMHV3R3EyTW01Tjl4ZzFiZHklMkZuSnlGSDZselduVjRHUVFIaDlMRnEzc0pkanI1UEJLMjZObTU0Smg5Zjc3dTVUN0hPdzhqQSUzRCUzRA..'
+            },
+            {
+                subtitle: 'Inside Asana',
+                url: 'https://asana.com/es/inside-asana'
+            },
+            {
+                subtitle: 'Relaciones con los inversores',
+                url: 'https://investors.asana.com/?_gl=1*1exerz4*_gcl_au*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*FPAU*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*_ga*MTM2OTA1MzA2MC4xNzI2MzExNDkw*_ga_J1KDXMCQTH*MTcyNjc2NTgyNS4yNC4xLjE3MjY3NjY0MTIuMC4wLjE4MjQ4OTE3MzU.*_fplc*WjBwRkclMkZFS1dZaUM1Q0s5allFc3BCaGZnWkdraHVubUpya3JONzE1OEFiZWtIM3pHTmlBR3NZMHV3R3EyTW01Tjl4ZzFiZHklMkZuSnlGSDZselduVjRHUVFIaDlMRnEzc0pkanI1UEJLMjZObTU0Smg5Zjc3dTVUN0hPdzhqQSUzRCUzRA..'
+            },
+            {
+                subtitle: 'Mapa del sitio',
+                url: 'https://asana.com/es/site-map'
+            }
+        ]
+    },
+    {
+        id: 'flowwork-solutions',
+        title: 'Soluciones para flujos de trabajo',
+        links : [
+            {
+                subtitle: 'Gestión de proyectos',
+                url: 'https://asana.com/es/uses/project-management'
+            },
+            {
+                subtitle: 'Gestión de objetivos',
+                url: 'https://asana.com/es/product/goals'
+            },
+            {
+                subtitle: 'Gestión ágil',
+                url: 'https://asana.com/es/uses/agile-management'
+            },
+            {
+                subtitle: 'Gestión de tareas',
+                url: 'https://asana.com/es/uses/task-management'
+            },
+            {
+                subtitle: 'Aumento de la productividad',
+                url: 'https://asana.com/es/uses/increase-productivity'
+            },
+            {
+                subtitle: 'Gestión del trabajo',
+                url: 'https://asana.com/es/uses/work-management'
+            },
+            {
+                subtitle: 'Planificación de proyectos',
+                url: 'https://asana.com/es/uses/project-planning'
+            },
+            {
+                subtitle: 'Listas de tareas pendientes',
+                url: 'https://asana.com/es/uses/to-do-list'
+            },
+            {
+                subtitle: 'Ver todos los casos de uso',
+                url: 'https://asana.com/es/uses'
+            },
+            {
+                subtitle: 'Ver todos los equipos',
+                url: 'https://asana.com/es/teams'
+            }
+        ]
+    },
+    {
+        id: 'resources',
+        title: 'Recursos',
+        links : [
+            {
+                subtitle: 'Centro de ayuda',
+                url: 'https://asana.com/es/guide'
+            },
+            {
+                subtitle: 'Foro',
+                url: 'https://forum.asana.com/?_gl=1*12hjrf4*_gcl_au*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*FPAU*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*_ga*MTM2OTA1MzA2MC4xNzI2MzExNDkw*_ga_J1KDXMCQTH*MTcyNjc2NTgyNS4yNC4xLjE3MjY3NjY0MTIuMC4wLjE4MjQ4OTE3MzU.*_fplc*WjBwRkclMkZFS1dZaUM1Q0s5allFc3BCaGZnWkdraHVubUpya3JONzE1OEFiZWtIM3pHTmlBR3NZMHV3R3EyTW01Tjl4ZzFiZHklMkZuSnlGSDZselduVjRHUVFIaDlMRnEzc0pkanI1UEJLMjZObTU0Smg5Zjc3dTVUN0hPdzhqQSUzRCUzRA..'
+            },
+            {
+                subtitle: 'Asistencia',
+                url: 'https://asana.com/es/support'
+            },
+            {
+                subtitle: 'Directorio de aplicaciones',
+                url: 'https://asana.com/es/apps'
+            },
+            {
+                subtitle: 'Desarrolladores y API',
+                url: 'https://developers.asana.com/docs/?_gl=1*5igsam*_gcl_au*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*FPAU*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*_ga*MTM2OTA1MzA2MC4xNzI2MzExNDkw*_ga_J1KDXMCQTH*MTcyNjc2NTgyNS4yNC4xLjE3MjY3NjY0MTIuMC4wLjE4MjQ4OTE3MzU.*_fplc*WjBwRkclMkZFS1dZaUM1Q0s5allFc3BCaGZnWkdraHVubUpya3JONzE1OEFiZWtIM3pHTmlBR3NZMHV3R3EyTW01Tjl4ZzFiZHklMkZuSnlGSDZselduVjRHUVFIaDlMRnEzc0pkanI1UEJLMjZObTU0Smg5Zjc3dTVUN0hPdzhqQSUzRCUzRA..'
+            },
+            {
+                subtitle: 'Socios',
+                url: 'https://asana.com/es/partners'
+            },
+            {
+                subtitle: 'Comunidad de Asana',
+                url: 'https://asana.com/es/community'
+            },
+            {
+                subtitle: 'Eventos',
+                url: 'https://events.asana.com/?_gl=1*5igsam*_gcl_au*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*FPAU*MTMwOTY0OTQ5LjE3MjYzMTE0ODk.*_ga*MTM2OTA1MzA2MC4xNzI2MzExNDkw*_ga_J1KDXMCQTH*MTcyNjc2NTgyNS4yNC4xLjE3MjY3NjY0MTIuMC4wLjE4MjQ4OTE3MzU.*_fplc*WjBwRkclMkZFS1dZaUM1Q0s5allFc3BCaGZnWkdraHVubUpya3JONzE1OEFiZWtIM3pHTmlBR3NZMHV3R3EyTW01Tjl4ZzFiZHklMkZuSnlGSDZselduVjRHUVFIaDlMRnEzc0pkanI1UEJLMjZObTU0Smg5Zjc3dTVUN0hPdzhqQSUzRCUzRA..'
+            },
+            {
+                subtitle: 'Asana para organizaciones sin fines de lucro',
+                url: 'https://asana.com/es/nonprofit'
+            },
+            {
+                subtitle: 'Asana para la educación',
+                url: 'https://asana.com/es/education'
+            },
+            {
+                subtitle: 'Accesibilidad',
+                url: 'https://asana.com/es/accessibility'
+            }
+        ]
+    },
+    {
+        id: 'learn',
+        title: 'Aprende',
+        links : [
+            {
+                subtitle: '11 estilos de liderazgo',
+                url: 'https://asana.com/es/resources/leadership-styles'
+            },
+            {
+                subtitle: '110 preguntas para romper el hielo',
+                url: 'https://asana.com/es/resources/icebreaker-questions-team-building'
+            },
+            {
+                subtitle: 'Consejos para el resumen ejecutivo',
+                url: 'https://asana.com/es/resources/executive-summary-examples'
+            },
+            {
+                subtitle: 'Consejos para combatir el síndrome del impostor',
+                url: 'https://asana.com/es/resources/impostor-syndrome'
+            },
+            {
+                subtitle: 'Cómo evitar el agotamiento',
+                url: 'https://asana.com/es/resources/what-is-burnout'
+            },
+            {
+                subtitle: 'Consejos sobre análisis FODA',
+                url: 'https://asana.com/es/resources/swot-analysis'
+            },
+            {
+                subtitle: '¿Qué son los objetivos y resultados clave (OKR)?',
+                url: 'https://asana.com/es/resources/okr-meaning'
+            },
+            {
+                subtitle: '¿Qué son los objetivos SMART?',
+                url: 'https://asana.com/es/resources/smart-goals'
+            },
+            {
+                subtitle: '¿Qué es la corrupción del alcance?',
+                url: 'https://asana.com/es/resources/what-is-scope-creep'
+            },
+            {
+                subtitle: 'Ver todos los recursos',
+                url: 'https://asana.com/es/resources'
+            }
+        ]
+    }
+]
